4c9b928b23f4aef8c74b195fc2029a1883b2595e
Eugene Makarenko <apple@MacBook-Pro.local>
2020-08-25 19:46:21 +0300

    add-winches-reducer

---
+++ src/reducers/app_settings/winches.js
@@ -0,0 +1,23 @@
+import { SET_WINCH_DATA, ADD_WINCH, REMOVE_WINCH } from "../../constants/config";
+import pattern from "./pattern.json";
+
+const { winches } = pattern;
+
+export default (state = winches, action) => {
+  switch (action.type) {
+    case SET_WINCH_DATA:
+      return state.map((winch) => (winch._id === action.winch._id ? action.winch : winch));
+
+    case ADD_WINCH:
+      const ObjectId = (m = Math, d = Date, h = 16, s = (s) => m.floor(s).toString(h)) =>
+        s(d.now() / 1000) + " ".repeat(h).replace(/./g, () => s(m.random() * h));
+
+      return [...state, { _id: ObjectId(), name: "", title: "" }];
+
+    case REMOVE_WINCH:
+      return state.filter((winch) => winch._id !== action._id);
+
+    default:
+      return state;
+  }
+};
