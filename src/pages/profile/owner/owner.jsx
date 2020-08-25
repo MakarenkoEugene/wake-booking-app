879612edeca951f5ffd6b4591ed04ff496e65392
Eugene Makarenko <apple@MacBook-Pro.local>
2020-08-25 19:51:53 +0300

    update-app-after-change-settings

--- src/pages/profile/owner/owner.jsx
+++ src/pages/profile/owner/owner.jsx
@@ -3,30 +3,48 @@ import ListSteps from "../../../components/form/steps";
 import "./style.scss";
 import FormCreate from "./create";
 import Settings from "./settings";
+import { connect } from "react-redux";
 
-export default function OwnerPage() {
-  return (
-    <div id="owner">
-      <ListSteps
-        steps={[
-          { text: "Основное" },
-          { text: "Время работы" },
-          { text: "Персонал" },
-          { text: "Описание" },
-          { text: "Доп. Опции" },
-          { text: "Внешний вид" },
-          { text: "Оплата" },
-        ]}
-      />
-      <h1>Owner</h1>
-      {/* <FormCreate /> */}
-      <Settings />
-    </div>
-  );
+const mapStateToProps = (store) => ({
+  config: store.appSettings,
+});
+const mapDispatchToProps = (dispatch) => ({});
+
+class OwnerPage extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.config !== this.props.config) {
+      frames.APP.postMessage({ config: this.props.config });
+    }
+  }
+
+  render() {
+    return (
+      <div id="owner">
+        <ListSteps
+          steps={[
+            { text: "Основное" },
+            { text: "Время работы" },
+            { text: "Персонал" },
+            { text: "Описание" },
+            { text: "Доп. Опции" },
+            { text: "Внешний вид" },
+            { text: "Оплата" },
+          ]}
+        />
+        <h1>Owner</h1>
+        {/* <FormCreate /> */}
+        <Settings />
+      </div>
+    );
+  }
 }
 
-{
-  /* <div id="select_leng">
+export default connect(mapStateToProps, mapDispatchToProps)(OwnerPage);
+/* <div id="select_leng">
 <p>Выберете язык который будет использоватся в програме:</p>
 <Select
   defaultValue={{ value: "en-US", label: "English" }}
@@ -34,4 +52,4 @@ export default function OwnerPage() {
   onChange={(e) => i18n.changeLanguage(e.value)}
 />
 </div> */
-}
+// }
