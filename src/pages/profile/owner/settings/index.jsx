799fe9869faf1591768e77af591b963342e290e7
Eugene Makarenko <apple@MacBook-Pro.local>
2020-08-25 19:51:00 +0300

    update-owner-page

--- src/pages/profile/owner/settings/index.jsx
+++ src/pages/profile/owner/settings/index.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import "./style.scss";
 import Select from "react-select";
+import InputToggle from "../../../../components/form/input_toggle";
 import SetWorkTime from "../../../../components/set_work_time";
+import SetWinches from "../../../../components/set_winches";
+import SetAdmins from "../../../../components/set_admins";
 
-export default function OwnerPage() {
+import { connect } from "react-redux";
+
+import { setShowIconsReserved } from "../../../../actions/config";
+
+const mapStateToProps = (store) => ({
+  showIconsReserved: store.appSettings.config.showIconsReserved,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setShowIconsReserved: (showIconsReserved) => dispatch(setShowIconsReserved(showIconsReserved)),
+});
+
+function Settings({ showIconsReserved, setShowIconsReserved }) {
   return (
     <div id="settings">
       <div>
         <p>
           {`Ета страница предназначина для владельцев и администраторов програмы WakeBooking. \nЗдесь находится настройка вашего экземпляра програмы WakeBooking.`}
         </p>
-        <h3>Ети параметры вы можете изменять в любое время по вашему желанию</h3>
+        <h4>Ети параметры вы можете изменять в любое время по вашему желанию</h4>
         <p>
           Ети изменения вступят в силу после нажатия клавиши "Подтвердить Изменения" после этого доступ к прошлой версии
           вашей програмы будет закрыт, всем клиентам которые используют вашу програму в данный момент нужно будет
@@ -21,6 +35,7 @@ export default function OwnerPage() {
         <div>
           <form id="form_settings">
             <div id="select_lent">
+              <h3>Languages</h3>
               <p>Выберете язык которые будет использоватся в програме:</p>
               <Select
                 defaultValue={{ value: "en", label: "English" }}
@@ -29,15 +44,42 @@ export default function OwnerPage() {
               />
             </div>
 
+            <SetWinches />
+
+            <SetAdmins />
+
+            <div>
+              <h3>Teachers</h3>
+            </div>
+
             <SetWorkTime />
 
-            <input type="datetime-local" />
+            <div>
+              <h3>Weekend</h3>
+            </div>
+
+            <div>
+              <h3>Price</h3>
+            </div>
+
+            <div>
+              <h3>Visual Appearance</h3>
+              <InputToggle
+                label={"Show Icons Reserved for teachers, winches and client: "}
+                checked={showIconsReserved}
+                onChangeChecked={setShowIconsReserved}
+              />
+            </div>
 
             <input type="submit" value="Подтвердить Изменения" />
           </form>
         </div>
-        <div></div>
+        <div>
+          <iframe name="APP" src="/app.html" scrolling="no" width="320" height="auto" frameBorder="0"></iframe>
+        </div>
       </section>
     </div>
   );
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
