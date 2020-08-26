5680433686d1f645ee265b52b266c8b2d77e549b
Eugene Makarenko <eugemakarenko@gmail.com>
2020-08-26 17:38:15 +0300

    get-client-for-teacher

--- website/src/middleware/app_settings.js
+++ website/src/middleware/app_settings.js
@@ -1,6 +1,6 @@
 import API from "./server_worker";
 import * as C from "../constants/app_settings";
-import { addAdmin } from "../actions/app_settings";
+import { addAdmin, addTeacher } from "../actions/app_settings";
 import { setLoading } from "../actions/index";
 
 export default () => (next) => async (action) => {
@@ -13,6 +13,21 @@ export default () => (next) => async (action) => {
         if (client.title !== "CLIENT_RIGHT") throw client;
 
         next(addAdmin({ name: client.name, phone: action.phone, _id: client._id, rights: ["SEE"] }));
+      } catch (error) {
+        alert(error.title ? error.title : error);
+      } finally {
+        next(setLoading(false));
+      }
+      break;
+
+    case C.GET_CLIENT_FOR_TEACHER:
+      try {
+        next(setLoading(true));
+
+        const client = await API.getClient(action.phone);
+        if (client.title !== "CLIENT_RIGHT") throw client;
+
+        next(addTeacher({ name: client.name, phone: action.phone, _id: client._id }));
       } catch (error) {
         alert(error.title ? error.title : error);
       } finally {
