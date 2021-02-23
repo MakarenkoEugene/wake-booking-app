3e367b4104a026b4654e2c62f136234ffc6a9a1e
Makarenko <pheka921@gmail.com>
2021-02-23 10:54:57 +0200

    init creative status

---
+++ src/app/components/creative-status/creative-status.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './creative-status.scss';
+
+const CreativeStatus = () => (
+  <div className='creative_status'>
+    <span className='badge'>PENDING</span>
+    <h2>We are working on your creative.</h2>
+    <p>We appreciate your feedback. We’ll upload a new version for your approval soon.</p>
+  </div>
+);
+
+export default CreativeStatus;
