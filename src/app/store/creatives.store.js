ce8e5d2d1c95f9b864b13bac9930f4b4d4d61090
Makarenko <pheka921@gmail.com>
2021-02-23 10:53:48 +0200

    create demo store

--- src/app/store/creatives.store.js
+++ src/app/store/creatives.store.js
@@ -2,19 +2,50 @@ import { http } from '@libs/http';
 import { makeAutoObservable } from 'mobx';
 
 export default class CreativesStore {
-  creatives = null;
+  data = {};
+
+  selectVersion = {};
+
+  orientation = 'portrait' // landscape, portrait
+
+  userDevice = '' // 'phone', ''
+
+  isOpen = true
 
   constructor() {
-    // this.hideModal = this.hideModal.bind(this);
+    this.onChangeOrientation = this.onChangeOrientation.bind(this);
+    this.onChangeIsOpen = this.onChangeIsOpen.bind(this);
+    this.reloard = this.reloard.bind(this);
 
     makeAutoObservable(this);
   }
 
   async get(id) {
-    console.log(id);
     const res = await http.get(`creatives/${id}`);
-    console.dir(res);
+    this.data = res;
 
-    this.creatives = res;
+    this.orientation = this.data?.defaultOrientation || 'portrait';
+    this.onSelectVersion(this.data?.demos[0] || null);
+  }
+
+  setUserDevice(device) {
+    this.userDevice = device;
+  }
+
+  onChangeIsOpen() {
+    this.isOpen = !this.isOpen;
+  }
+
+  onSelectVersion(version) {
+    this.selectVersion = version;
+    this.isOpen = true;
+  }
+
+  onChangeOrientation() {
+    this.orientation = this.orientation === 'portrait' ? 'landscape' : 'portrait';
+  }
+
+  reloard() {
+    this.onSelectVersion(null);
   }
 }
