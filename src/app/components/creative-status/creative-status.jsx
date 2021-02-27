882f5f463147db3a0e483d9d5b00914a8de00902
Makarenko <pheka921@gmail.com>
2021-02-27 15:20:10 +0200

    creative status hendler

--- src/app/components/creative-status/creative-status.jsx
+++ src/app/components/creative-status/creative-status.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { inject, observer } from 'mobx-react';
 import './creative-status.scss';
+import { Approved, Pending, Paused, Live, ResponseError, Thanks } from './info-status';
+import { InternalReview } from './internal-review';
+import { InternalPAReview } from './internal-pa-review';
+import { InternalPALabels } from './internal-pa-labels';
+import { InternalIECReview } from './internal-iec-rewiev';
 
-const CreativeStatus = ({ rootStore: { creatives } }) => {
-  if (!creatives?.data) return null;
+const CreativeStatus = ({ rootStore: { creatives }, className }) => {
+  // TODO remove
+  const stateConfigs = [
+    { state: ['internal-pa-labels'], Component: InternalPALabels, props: creatives.data, labels: true },
+    { state: ['internal-iec-rewiew'], Component: InternalIECReview, props: creatives.data },
+    { state: ['internal-pa-review'], Component: InternalPAReview },
+    { state: ['internal-review'], Component: InternalReview },
+    { state: ['approved'], Component: Approved, info: true },
+    { state: ['pending'], Component: Pending, info: true },
+    { state: ['paused'], Component: Paused, info: true },
+    { state: ['live'], Component: Live, info: true },
+    { state: ['error'], Component: ResponseError, info: true },
+    { state: ['thanks'], Component: Thanks, info: true },
+    { state: ['thanks-iec-rewiev'],
+      Component: Thanks,
+      info: true,
+      props: { message: '1/2 responded \n1 approved\n 0 disapproved' } },
+  ];
+
+  const config = stateConfigs.find(({ state }) => state.includes(creatives.state));
+
+  const { Component, info, labels, props } = config || {};
+
+  useEffect(() => {
+    creatives.setIsInfoState(info);
+  }, [info]);
+
+  if (!Component || !creatives.data) return null;
 
   return (
-    <div className='creative_status'>
-      <span className='badge'>PENDING</span>
-      <h2>We are working on your creative.</h2>
-      <p>We appreciate your feedback. We’ll upload a new version for your approval soon.</p>
+    <div className={`creative_status_wrapper ${info ? 'info' : ''} ${className}`}>
+      { !info && (labels ? <h4>LABELS:</h4> : <h4>FEEDBACK:</h4>)}
+      <div className='creative_status'>
+        <Component {...props} />
+      </div>
     </div>
   );
 };
