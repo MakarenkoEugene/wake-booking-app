af72e1b967671cd6ca8eb354db473568828cc1b7
Makarenko <pheka921@gmail.com>
2021-03-29 16:12:39 +0300

    update TODO

---
+++ src/app/utils/index.js
@@ -0,0 +1,9 @@
+export const replaceEmoji = (text) => text
+  .replace('🙂', ':)')
+  .replace('😞', ':(')
+  .replace('😛', ':P')
+  .replace('😃', ':D')
+  .replace('😉', ';)')
+  .replace('👍', '(y)')
+  .replace('👎', '(n)')
+  .replace('💡', '(on)');
